97454746743cfa4f11281c5f49f1e399ab38b4ad
phuttonn <prestonhutton93@gmail.com>
2022-11-02 21:10:33 -0600

    Add comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1; // set randomNumber to a number between 1 and 100
 
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
+// stores references to results paragraph in HTML
+const guesses = document.querySelector('.guesses'); 
+const lastResult = document.querySelector('.lastResult'); 
+const lowOrHi = document.querySelector('.lowOrHi'); 
 
+// stores references to the form text input and submit button 
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 
-let guessCount = 1;
+let guessCount = 1; // initialize guessCount at 1
 let resetButton;
